eb9931a0cb7eede9d27a2af967d3059d29f8c184
Nischal <nyyscal541556@gmail.com>
2025-09-01 21:12:09 +0545

    File Based Routing

---
+++ src/app/_layout.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='(shop)' options={{headerShown:true, title:"Shop"}}/>
+      <Stack.Screen name='categories' options={{headerShown:true, title:"Categories"}}/>
+      <Stack.Screen name='product' options={{headerShown:true, title:"Product"}}/>
+      <Stack.Screen name='cart' options={{presentation:"modal", title:"Shopping Cart"}}/>
+      <Stack.Screen name='auth' options={{headerShown:true}}/>
+    </Stack>
+  )
+}
+
+export default RootLayout
